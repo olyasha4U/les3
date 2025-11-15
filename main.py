79187cd98ff0cb2b0f780e6b374ca0af97bda582
savicbu17-beep <savic.bu17@gmail.com>
2025-11-15 09:31:59 +0200

    grery

---
+++ main.py
@@ -0,0 +1,29 @@
+# створює ітератор
+a = iter([1,2, 3,4, 5])
+
+# отрмати наступний елемент
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+try:
+    print(next(a))
+except:
+    print("Error")
+# ітерований об'єкт
+class Counter:
+    def __init__(self):
+        self.a = 1
+
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        self.a *= 1
+        return self.a
+
+c = iter(Counter())
+for i in range(50):
+    print(next(c))
