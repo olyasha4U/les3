128407ce03b573098f6545ff7d1cc3e33e052ca3
savicbu17-beep <savic.bu17@gmail.com>
2025-11-15 10:26:41 +0200

    grery3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# створює ітератор
+# створює ітератор(нескінченний)
 a = iter([1,2, 3,4, 5])
 
 # отрмати наступний елемент
@@ -11,7 +11,7 @@ try:
     print(next(a))
 except:
     print("Error")
-# ітерований об'єкт
+# ітерований об'єкт (який повторюється, скінчений)
 class Counter:
     def __init__(self):
         self.a = 1
@@ -27,3 +27,26 @@ class Counter:
 c = iter(Counter())
 for i in range(50):
     print(next(c))
+
+#генератори
+def gen(number):
+    b = number
+    i = 0
+    while True:
+        i += 1
+        b += 1
+        yield b
+g =gen(5)
+print(next(g))
+print(next(g))
+print(next(g))
+#декоратори
+def dec(f1):
+    def f2():
+        result=f1()
+        return f"Result:{result}"
+    return f2
+@dec
+def test():
+    return "Test"
+print(test())
